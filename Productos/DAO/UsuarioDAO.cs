aebce1b4406ba1ad904f3201d1afb847f0cd4316
ClaudiaLiraB <clucialirab@uamv.edu.ni>
2025-09-29 10:54:55 -0600

    clase 29/9

---
+++ Productos/DAO/UsuarioDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos.DAO
+{
+    public class UsuarioDAO
+    {
+        public String[] users = { "admin", "user1", "user2" };
+        public String[] pws = { "admin123", "user123", "user223" };
+
+        public Boolean Validar(String user, String pw)
+        {
+            for(int i = 0; i < users.Length; i++)
+            {
+                if(users[i] == (user) && (pws[i].Equals(pw)))
+                {
+                    return true;
+                }
+            }return false;
+        }
+    }
+}
